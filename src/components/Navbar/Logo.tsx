e9d3476d43efe8512ee4418001f5d393ed2b7214
josiasdev <josiasmartins098@gmail.com>
2025-05-05 15:13:45 -0300

    feat: Componente logotipo

---
+++ src/components/Navbar/Logo.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import logotipo from "../Assets/images/logotipo.svg"
+
+
+export default function Logo() {
+    return (
+        <div className={`flex items-center gap-2`}>
+            <Link href="/" className={`flex items-center gap-2`}>
+                <Image src={logotipo} width={30.04} height={25.33} alt="Logo" priority />
+                <span className={`text-[#109DAD] font-semibold text-lg`}>Brasil Cursinhos</span>
+            </Link>
+        </div>
+    )
+}
