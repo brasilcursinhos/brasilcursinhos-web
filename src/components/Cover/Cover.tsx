f192a452041acf73df5b442ee390e44fdf5825e9
josiasdev <josiasmartins098@gmail.com>
2025-05-05 15:15:09 -0300

    feat: Parte do mapa e imagens

---
+++ src/components/Cover/Cover.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import Image from 'next/image'
+import MapaBrasil from "../Assets/images/mapa-brasil.svg"
+import Pessoa1 from "../Assets/images/pessoa1.svg"
+import Pessoa2 from "../Assets/images/pessoa2.svg"
+import Pessoa3 from "../Assets/images/pessoa3.svg"
+import Pessoa4 from "../Assets/images/pessoa4.svg"
+import Pessoa5 from "../Assets/images/pessoa5.svg"
+
+export default function Cover() {
+  return (
+    <section className="flex items-center justify-center px-10 py-20 gap-20 bg-[#f2f9fb] overflow-hidden">
+      
+      {/* Mapa + Pessoas */}
+      <div className="relative">
+        <Image 
+          src={MapaBrasil}
+          alt="Mapa do Brasil" 
+          className="object-contain" 
+          width={445.47}
+          height={458.29}
+        />
+
+        {/* Pessoas */}
+        <div className="absolute top-6 left-16">
+          <Image src={Pessoa1} alt="Pessoa 1" width={104.97} height={104.78} className="rounded-full" />
+        </div>
+
+        <div className="absolute bottom-40 left-2">
+          <Image src={Pessoa2} alt="Pessoa 2" width={100.82} height={100.64} className="rounded-full" />
+        </div>
+
+        <div className="absolute top-28 left-45">
+          <Image src={Pessoa3} alt="Pessoa 3" width={109.95} height={109.75} className="rounded-full" />
+        </div>
+
+        <div className="absolute top-20 right-1 ">
+          <Image src={Pessoa4} alt="Pessoa 4" width={107.85} height={107.65} className="rounded-full" />
+        </div>
+
+        <div className="absolute bottom-30 right-2">
+          <Image src={Pessoa5} alt="Pessoa 5" width={100.12} height={99.95} className="rounded-full" />
+        </div>
+      </div>
+
+      {/* Texto */}
+      <div className="max-w-xl">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4 leading-tight">
+          Nós representamos, <br />
+          desenvolvemos e integramos <br />
+          os cursinhos populares do país
+        </h1>
+        <p className="text-[#109DAD] mt-4">
+          Trazemos soluções para fortalecer e potencializar o Movimento <br/> dos Cursinhos Populares em todo o Brasil e democratizar o<br/> acesso ao Ensino Superior.
+        </p>
+      </div>
+    </section>
+  )
+}
