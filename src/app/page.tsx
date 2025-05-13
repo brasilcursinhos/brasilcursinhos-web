26b52a5cc51ede55d72445df7bb9d0d7155a66c3
josiasdev <francisco.batista67@alu.ufc.br>
2025-05-13 11:25:18 -0300

    feat: adicionando componente resultados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Carrosel from "@/components/Carrosel/Carrosel"
 import Cover from "@/components/Cover/Cover"
 import Navbar from "@/components/Navbar/Navbar"
+import Resultados from "@/components/Resultados/Resultados"
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
      <Navbar/>
      <Cover/>
      <Carrosel/>
+     <Resultados/>
     </main>
-    
   )
 }
