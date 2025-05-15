dce3ffa6c993d6f084f2ec218a8afbee5d57c905
josiasdev <francisco.batista67@alu.ufc.br>
2025-05-15 10:16:24 -0300

    update: atualizando o import da logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import Logo from './Logo'
-import Link from 'next/link'
-import { useState } from 'react'
+import Logo from './Logo';
+import Link from 'next/link';
+import { useState } from 'react';
 
 
 
