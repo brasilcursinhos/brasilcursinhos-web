251fc379cd633df62a4fea30f84b994e15e2b327
josiasdev <francisco.batista67@alu.ufc.br>
2025-06-25 09:28:09 -0300

    feat: Tela de contatos onde pode enviar contato e aparece um modal.

---
+++ src/components/Contato/Contato.tsx
@@ -0,0 +1,124 @@
+'use client'
+
+import Image from 'next/image'
+import React from 'react'
+import Contato_image from "@/components/Assets/imagens/contato/contato.svg"
+import Sucesso from "@/components/Assets/imagens/contato/sucesso.svg"
+
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogAction,
+} from "@/components/ui/alert-dialog"
+import { useState } from 'react'
+
+export default function Contato() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [mensagem, setMensagem] = useState('');
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  }
+  const handleSendAnotherMessage = () => {
+    setNome('');
+    setEmail('');
+    setMensagem('');
+    window.location.reload();
+  }
+    const isFormValid = nome.trim() !== '' && email.trim() !== '' && mensagem.trim() !== '';
+
+
+  return (
+    <section className="flex flex-col lg:flex-row items-center justify-center px-6 py-20 gap-12 bg-[#fffff] text-[#5F7691]">
+      <div className="w-full lg:w-1/2 flex justify-center">
+        <Image src={Contato_image} alt="Ilustração de contato" className="max-w-md w-full h-auto" />
+      </div>
+
+      <div className="w-full lg:w-1/2 max-w-lg">
+        <h2 className="text-[#E16919] text-xl font-semibold mb-1">Contato</h2>
+        <h1 className="text-3xl font-bold text-[#3D4C62] mb-4">
+          Vem conversar com a gente!
+        </h1>
+        <p className="text-[#5F7691] mb-6">
+          A Fernanda ou o João, nossos presidentes, vão responder sua mensagem. Tire suas dúvidas ou marque uma conversa com eles.
+        </p>
+
+        <AlertDialog>
+          <form className="space-y-5" onSubmit={handleSubmit}>
+            <div>
+              <label className="block text-sm font-semibold text-[#5F7691] mb-1">
+                Seu nome<span className="text-[#E16919]">*</span>
+              </label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 bg-transparent border border-gray-600 rounded-md focus:outline-none focus:border-teal-400"
+                placeholder="Digite seu nome"
+                required
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-semibold text-[#5F7691] mb-1">
+                Seu e-mail<span className="text-[#E16919]">*</span>
+              </label>
+              <input
+                type="email"
+                className="w-full px-4 py-2 bg-transparent border border-gray-600 rounded-md focus:outline-none focus:border-teal-400"
+                placeholder="Digite seu e-mail"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-semibold text-[#5F7691] mb-1">
+                Sua mensagem<span className="text-[#E16919]">*</span>
+              </label>
+              <textarea
+                className="w-full h-40 px-4 py-2 bg-transparent border border-gray-600 rounded-md resize-none focus:outline-none focus:border-teal-400"
+                placeholder="Escreva sua mensagem"
+                required
+                value={mensagem}
+                onChange={(e) => setMensagem(e.target.value)}
+              />
+            </div>
+            <AlertDialogTrigger asChild disabled={!isFormValid}>
+              <button className="bg-[#109DAD] text-[#FFFFFF] px-6 py-3 rounded-xl text-sm font-semibold cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed">
+                Enviar Mensagem
+              </button>
+            </AlertDialogTrigger>
+          </form>
+          <AlertDialogContent className="flex flex-col items-center justify-center text-center ">
+            <AlertDialogHeader>
+              <div className="flex justify-center">
+                <Image src={Sucesso} alt="Sucesso" width={80} height={80} />
+              </div>
+
+              <AlertDialogTitle className="text-[#3D4C62] text-center">
+                Mensagem enviada com sucesso!
+              </AlertDialogTitle>
+
+              <AlertDialogDescription className="text-[#5F7691] text-center">
+                Ficamos muito felizes com o seu contato e vamos responder assim que possível.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+
+            <AlertDialogFooter className="flex justify-center">
+              <AlertDialogAction className="bg-[#109DAD] hover:bg-[#0a7f8a] cursor-pointer" onClick={handleSendAnotherMessage}>
+                Enviar outra mensagem
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </div>
+    </section>
+  )
+}
